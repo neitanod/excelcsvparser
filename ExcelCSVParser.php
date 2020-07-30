b29560d07411105e5b2231ef6613d57147973670
Sebastián Grignoli <grignoli@gmail.com>
2020-07-30 19:49:30 -0300

    A bit of modernization

--- ExcelCSVParser.php
+++ ExcelCSVParser.php
@@ -1,9 +1,46 @@
 <?php
 /*
+Copyright (c) 2006 Sebastián Grignoli
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. Neither the name of copyright holders nor the names of its
+   contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL COPYRIGHT HOLDERS OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/**
+ * @author   "Sebastián Grignoli" <grignoli@gmail.com>
+ * @package  ExcelCSVParser
+ * @version  1.0
+ * @link     https://github.com/neitanod/excelcsvparser
+ * @license  Revised BSD
+  */
+
+/*
 Example:
 
 $csvfile = new ExcelCSVParser("file.csv");
-while(!$csvfile->eof){
+while(!$csvfile->eof()){
     $line = $csvfile->parseLine();
     $values[] = $line;
 }
@@ -37,7 +74,7 @@ class ExcelCSVParser {
     var $escape = '"';
 
     // auxiliar values
-    protected $file;
+    protected $file = null;
     protected $enclosed;
     protected $eof = false;
 
@@ -51,6 +88,12 @@ class ExcelCSVParser {
             $this->eof = feof($this->file);
         }
         return $this;
+    }
+
+    public function reset() {
+        if ( !is_null($this->file) ) {
+            fseek($this->file, 0);
+        }
     }
 
     public function eof(){
@@ -87,7 +130,7 @@ class ExcelCSVParser {
         return $parts;
     }
 
-    function get_csv_value($line, $already_enclosed = false){
+    protected function get_csv_value($line, $already_enclosed = false){
         // auxiliar function for internal use
         // returns an array consisting of:
         //        "value" => <the_first_value_of_the_line>
@@ -108,7 +151,8 @@ class ExcelCSVParser {
         if($enclosed){
             $output = str_replace($this->escape.$this->delimiter, "{[_delimiter_character_in_contents_]}", $output);
             $output = str_replace($this->escape.$this->escape, "{[_escape_character_in_contents_]}", $output);
-            if(substr($output, -1) == $this->delimiter){
+            if(substr($output, -1) == $this->delimiter ||
+               substr($output, -2) == $this->delimiter."\n"){
                 $output = substr($output, 0, -1);
             } elseif(!empty($line)) {
                 $res = $this->get_csv_value($line, true);
@@ -132,7 +176,7 @@ class ExcelCSVParser {
         return array("value" => $output, "remaining" => $line);
     }
 
-    function str_ibefore($haystack, $limit)
+    protected function str_ibefore($haystack, $limit)
     {
         // auxiliar function for internal use
         // returns string from the begining of haystack
